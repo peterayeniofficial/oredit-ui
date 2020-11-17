d70a6b6f5c8200cabb508635b061aa47fc7c4f41
Peter Ayeni <peter.ayeni@previsico.com>
2020-11-17 21:30:22 +0000

    chore: added graphql generator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
+import { createClient, Provider } from 'urql';
+
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
 
 import theme from '../theme';
 
+const client = createClient({ url: 'http://localhost:9000/graphql' });
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider options={{}}>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider options={{}}>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,15 +3,29 @@ import { Form, Formik } from 'formik';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/inputField';
 import { Box, Button } from '@chakra-ui/core';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../../utils/toErrorMap';
+import { useRouter } from 'next/router';
+
 interface registerProps {}
 
 export const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useRegisterMutation();
+  const router = useRouter();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
-          console.table(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          console.log('response', response.data?.register.errors);
+
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // works
+            router.push('/');
+          }
         }}
       >
         {({ isSubmitting }) => (
